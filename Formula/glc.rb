c84701fb4daa9a647b746b5fae5068bf2eca413d
YoungTaek Joo <soomtong@gmail.com>
2026-05-22 17:01:36 +0900

    Bump glc to v0.5.1

--- Formula/glc.rb
+++ Formula/glc.rb
@@ -1,9 +1,9 @@
 class Glc < Formula
   desc "Git history file viewer - TUI for exploring files from git history"
   homepage "https://github.com/soomtong/gluck"
-  url "https://github.com/soomtong/gluck/releases/download/v0.5.0/glc-arm64-darwin.tar.gz"
-  version "0.5.0"
-  sha256 "e0135ee9e18d5799ff6d84ab5a5db8abe21d5fc9ca190f13bdb2f5c94e37506d"
+  url "https://github.com/soomtong/gluck/releases/download/v0.5.1/glc-arm64-darwin.tar.gz"
+  version "0.5.1"
+  sha256 "078af040d6ddafba24e4e4f85fa6ca388d35a90a63b3fa0f839b1f5a0ce2e5a8"
   license "MIT"
 
   def install
