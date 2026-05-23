c72997755d862f5aec504e9b8f2d1b6120c2f252
YoungTaek Joo <soomtong@gmail.com>
2026-05-23 16:07:36 +0900

    Fix glc 0.5.5 sha256 - correct arm64 binary inside tar.gz

--- Formula/glc.rb
+++ Formula/glc.rb
@@ -3,7 +3,7 @@ class Glc < Formula
   homepage "https://github.com/soomtong/gluck"
   url "https://github.com/soomtong/gluck/releases/download/v0.5.5/glc-0.5.5-darwin-aarch64.tar.gz"
   version "0.5.5"
-  sha256 "e61d953fb77b26a979c8e1060dd96d46de5b171c12a031bbe1dcae8654c20d25"
+  sha256 "82f904d4413964c815d753eb8ac4e89573a6e7c98799dc6e420622481457ea48"
   license "MIT"
 
   def install
