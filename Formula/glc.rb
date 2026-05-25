e16e532e7b9f6cf54a2eb908e89d6dcaed8e0032
soomtong <soomtong@users.noreply.github.com>
2026-05-25 13:07:42 +0000

    Update glc to v0.8.1

--- Formula/glc.rb
+++ Formula/glc.rb
@@ -1,9 +1,9 @@
 class Glc < Formula
   desc "Git history file viewer - TUI for exploring files from git history"
   homepage "https://github.com/soomtong/gluck"
-  url "https://github.com/soomtong/gluck/releases/download/v0.7.4/glc-0.7.4-darwin-aarch64.tar.gz"
-  version "0.7.4"
-  sha256 "8f0bbfc673f34e1b5ec49df18f3fdaede539482d57d9e4aeb51d7fe0f00a093e"
+  url "https://github.com/soomtong/gluck/releases/download/v0.8.1/glc-0.8.1-darwin-aarch64.tar.gz"
+  version "0.8.1"
+  sha256 "085f46460becdb3954b8018f5a9e4422839c36ae816b813b6dae422178f544ca"
   license "MIT"
 
   def install
